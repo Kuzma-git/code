4b4871993ac124e654464fb3ae26153248c66191
Kuzma <Kuzma@milo.net>
2020-06-23 19:09:53 +0300

    last update lalala

---
+++ main.js
@@ -0,0 +1,22 @@
+var test1 = 'test1';
+let test2 = 'test2';
+const test3 = 'test3';
+
+
+test1 = 10;
+test2 = 893753;
+//test3= 4353;
+
+
+
+/*
+var - origin
+
+let - not constant 
+cont - constant variable
+
+Primitives:
+string - 'content', "content"
+number - 12345, NaN, Infinty
+boolean - true, false
+*/
